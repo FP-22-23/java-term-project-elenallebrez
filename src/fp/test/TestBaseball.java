67aa284926293aba9de7187708ffbbe0a5a7af72
elenallebrez <116157269+elenallebrez@users.noreply.github.com>
2023-05-19 23:27:24 +0200

    Update TestBaseball.java

--- src/fp/test/TestBaseball.java
+++ src/fp/test/TestBaseball.java
@@ -7,22 +7,33 @@ import baseball.Types;
 public class TestBaseball {
 	
 	public static void main(String[] args) {
-		Baseball new1 = new Baseball("CU",2019-8-10,45.5,"Pablo Fdez", "null","walks",3,"R","Boston","NY");
-		Baseball new2 = new Baseball("FF",2021-1-12,50.5,"Carmen Perez", "strikeout","walks",2,"R","NY","Texas");
-		Baseball new3 = new Baseball("CH",2020-9-11,30.5,"Nacho Gonzalez", "homerun","run",4,"L","Chicago","Celties");
-		Baseball new4 = new Baseball("CU",2019-8-10,45.5,"Pablo Fdez", "null","walks",3,"R","Boston","NY");							
-		Baseball new5 = new2;
-	
-		System.out.println(new1);
-		System.out.println("Are new1 and new2 equals? "+ new1.equals(new4));
-		System.out.println("Are new1 and new5 equals? "+ new1.equals(new5));
+		//We will first try to test the Baseball Class
+		Baseball b1 = new Baseball(Types.CU,LocalDate.of(2019, 8, 9),45.5,"Pablo Fdez", "null","walks",3,true,"Boston","NY");
+		Baseball b2 = new Baseball(Types.FF,LocalDate.of(2021, 11, 12),50.5,"Carmen Perez", "strikeout","walks",2,true,"NY","Texas");
+		Baseball b3 = new Baseball(Types.CH,LocalDate.of(2020, 9, 11),30.5,"Nacho Gonzalez", "homerun","run",4,false,"Chicago","Celties");
+		Baseball b4 = new Baseball(Types.CU,LocalDate.of(2019, 5, 3),45.5,"Pablo Fdez", "null","walks",3,true,"Boston","NY");							
+		Baseball b5 = b2;
 		
-		new3.setPlayer("Miguel Sanchez");
-		System.out.println(new3);
+		//Now we test the second constructor
+		Baseball b6 = new Baseball(Types.SL,LocalDate.of(2017, 1, 2),15.7,"Pablo Fdez", "homerun","NY","Boston" );
+		System.out.println(b6);
 		
-		new2.setHome_team("Javier Lopez");
-		System.out.println(new2);
+		//Getters and setters
+		b3.setSpeed(30.0);
+		System.out.println("The ball speed of " + b3.getPlayer() +" is: "+ b3.getSpeed());
+		System.out.println(b3);
+		
+		b2.setHome_team("Californa");
+		System.out.println("The home team of the match played at "+ b2.getDate()+ "is " + b2.getHome_team());
+		System.out.println(b2);
+		
+		System.out.println(b1.getTotalDescription());
+		
+		//We now test for equality and Natural Order
+		System.out.println("Are new1 and new2 equals? "+ b1.equals(b4));
+		System.out.println("Are new1 and new5 equals? "+ b1.equals(b5));
+		System.out.println(b1.compareTo(b2));
+
 	}
 	
-
 }
